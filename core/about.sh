58045842c50bc5a1da6a049f46b1236ae20bbdc3
Anonymoush4cker7 <65849213+Anonymoush4cker7@users.noreply.github.com>
2020-07-15 18:37:09 +0530

    Add files via upload

---
+++ core/about.sh
@@ -0,0 +1,24 @@
+clear
+echo '                    
+            
+              ████████████████████████████
+              █─█─█▄─▄▄─█▄─▄███▄─▄███─▄▄─█
+              █─▄─██─▄█▀██─██▀██─██▀█─██─█
+              ▀▄▀▄▀▄▄▄▄▄▀▄▄▄▄▄▀▄▄▄▄▄▀▄▄▄▄▀
+                                                 
+' | lolcat
+echo " "
+echo "                         About"|lolcat
+echo " "
+echo " Hey, there I am Anonymous h4cker7, i create this tool
+         for metasploit users, so i hope guys you
+                      liked it. 😘"
+echo ""
+echo "            [•] You Tube = Hacker_terminal"| lolcat
+echo "            [•] Telegram = @Hacker_terminal"| lolcat
+echo "            [•] Instagram = hacker_terminal"| lolcat
+echo " "
+
+sleep 10.0
+cd $HOME/MSF
+bash msf.sh
---
+++ core/update.sh
@@ -0,0 +1,47 @@
+#colour section
+red='\033[1;31m'
+rset='\033[0m'
+grn='\033[1;32m'
+ylo='\033[1;33m'
+#script coding starts
+clear
+echo " "
+echo " "
+echo '
+                  
+█░░█ █▀▀█ █▀▀▄ █▀▀█ ▀▀█▀▀ █▀▀ 
+█░░█ █░░█ █░░█ █▄▄█ ░░█░░ █▀▀ 
+░▀▀▀ █▀▀▀ ▀▀▀░ ▀░░▀ ░░▀░░ ▀▀▀ 
+'|lolcat
+echo " "
+echo " "
+sleep 6.0
+clear
+echo -e "$red                         ChEcKiNg..>$rset"
+sleep 2.0
+clear
+echo -e "$red                         ChEcKiNg...>$rset"
+sleep 2.0
+clear
+echo -e "$red                         ChEcKiNg....>$rset"
+sleep 2.0
+clear
+echo -e "$red                         ChEcKiNg.....>$rset"
+sleep 2.0
+clear
+echo " "
+echo " "
+echo -e "$grn                updating the MSF please wait$rset"
+sleep 2.0
+cd $HOME
+rm -rf MSF
+git clone https://github.com/Anonymoush4cker7/MSF
+clear
+echo " "
+echo -e "$grn               MSF has been updated check it now...$rset"
+sleep 3.0
+echo " "
+cd $HOME/MSF
+bash msf.sh
+clear
+sleep 5.0
